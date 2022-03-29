32cee252796dd51464889530053c286fb981585f
likuny0415 <li.kuny@husky.neu.edu>
2022-03-29 21:27:46 +0800

    feature: modal form

--- components/AddTodoForm.tsx
+++ components/AddTodoForm.tsx
@@ -10,14 +10,21 @@ export const AddTodoForm: React.FC<Props> = ({ addTodo }: Props) => {
   const [ddl, setDdl] = useState(0);
   const [priority, setPriority] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    addTodo(text, ddl, priority);
+    setText("");
+    const myModal = document.getElementById("my-modal") as HTMLInputElement;
+    myModal.checked = false;
+  }
+
   const currentDate = dayjs().format("YYYY-MM-DDThh:mm");
 
-  return (
-    <form className="bg-gray-200 grid rounded justify-center" onSubmit={(e) => {
-        e.preventDefault();
-        addTodo(text, ddl, priority);
-        setText("")
-      }}>
+  const TodoForm = (
+    <form
+      className="bg-gray-200 grid rounded justify-center"
+      onSubmit={handleSubmit}
+    >
       <label htmlFor="todo" className="label justify-center">
         Your Todo
       </label>
@@ -27,7 +34,6 @@ export const AddTodoForm: React.FC<Props> = ({ addTodo }: Props) => {
         value={text}
         className="input input-bordered w-full max-w-xs"
         onChange={(e) => {
-            console.log(text)
           setText(e.target.value);
         }}
         required
@@ -38,30 +44,46 @@ export const AddTodoForm: React.FC<Props> = ({ addTodo }: Props) => {
       <input
         id="ddl"
         type="datetime-local"
-        value={currentDate}
+        defaultValue={currentDate}
         className="input input-bordered w-full max-w-xs "
         onChange={(e) => {
           setDdl(Date.parse(e.target.value));
         }}
         required
       />
-      <label htmlFor="priority" className="label" >Priority of Todo</label>
-      <select id="priority" defaultValue={'Medium'}  className="select w-full max-w-xs" onChange={(e) => {
-          setPriority(e.target.value)          
-      }} required>
-
+      <label htmlFor="priority" className="label">
+        Priority of Todo
+      </label>
+      <select
+        id="priority"
+        defaultValue={"Medium"}
+        className="select w-full max-w-xs"
+        onChange={(e) => {
+          setPriority(e.target.value);
+        }}
+        required
+      >
         <option value="High">High</option>
         <option value="Medium">Medium</option>
         <option value="Low">Low</option>
       </select>
-      <button
-        type="submit"
-        className="btn"
-        
-      >
+      <button type="submit" className="btn my-2">
         <span>Add Todo</span>
       </button>
-     
     </form>
+  );
+
+  return (
+    <>
+      <label htmlFor="my-modal" className="btn modal-button">
+        open modal
+      </label>
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <label htmlFor="my-modal" className="modal cursor-pointer ">
+        <label className="modal-box relative bg-red-50" htmlFor="">
+          {TodoForm}
+        </label>
+      </label>
+    </>
   );
 };
