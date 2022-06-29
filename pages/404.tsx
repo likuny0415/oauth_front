ff0032fc7a480988b64ea3ce2550045d8bf44aca
likuny0415 <li.kuny@husky.neu.edu>
2022-06-28 21:21:26 -0400

    test

--- pages/404.tsx
+++ pages/404.tsx
@@ -15,6 +15,7 @@ export default function Custom404() {
 
           <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
             <Link href="/">Go Home</Link>
+            
           </span>
         </a>
       </button>
